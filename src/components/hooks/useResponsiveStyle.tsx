5d1819ba195c93bb91672aec38d685e274c4d682
Huy Le <lequanghuylc@gmail.com>
2021-05-23 01:29:13 +0700

    add useDynamicResponsiveValue, xxl breakpoint

--- src/components/hooks/useResponsiveStyle.tsx
+++ src/components/hooks/useResponsiveStyle.tsx
@@ -16,6 +16,7 @@ export interface IResponsiveLayoutProps {
     md?: string | undefined,
     lg?: string | undefined,
     xl?: string | undefined,
+    xxl?: string | undefined,
     [breakpoint: string]: string | undefined,
   },
   xs?: string | undefined,
@@ -23,17 +24,19 @@ export interface IResponsiveLayoutProps {
   md?: string | undefined,
   lg?: string | undefined,
   xl?: string | undefined,
+  xxl?: string | undefined,
 }
 
 export const getResponsiveLayout = ( props : IResponsiveLayoutProps ) => {
-  const { responsiveLayout, xs, sm, md, lg, xl } = props;
+  const { responsiveLayout, xs, sm, md, lg, xl, xxl } = props;
   if (!!responsiveLayout) return responsiveLayout;
-  const shortObj = { xs, sm, md, lg, xl };
+  const shortObj = { xs, sm, md, lg, xl, xxl };
   if (typeof shortObj.xs === 'undefined') delete shortObj.xs;
   if (typeof shortObj.sm  === 'undefined') delete shortObj.sm;
   if (typeof shortObj.md  === 'undefined') delete shortObj.md;
   if (typeof shortObj.lg  === 'undefined') delete shortObj.lg;
   if (typeof shortObj.xl  === 'undefined') delete shortObj.xl;
+  if (typeof shortObj.xxl  === 'undefined') delete shortObj.xxl;
   return Object.keys(shortObj).length > 0 ? shortObj : undefined;
 }
 
@@ -61,6 +64,7 @@ interface IResponsiveRule<T> {
   md?: T | undefined,
   lg?: T | undefined,
   xl?: T | undefined,
+  xxl?: T | undefined,
   [breakpoint: string]: T | undefined,
 }
 
@@ -75,6 +79,7 @@ export const getResponsiveRule : GetResponsiveRule<any> = (viewportWidth, rules)
       md: 768,
       lg: 992,
       xl: 1200,
+      xxl: 1400,
     };
     for (let customBreakpoint in rules) {
       if (typeof minWidthBreakpoints[customBreakpoint] === 'number') continue;
@@ -106,6 +111,30 @@ export const getResponsiveRule : GetResponsiveRule<any> = (viewportWidth, rules)
     const responsiveRule = rules[currentBreakpoint];
     return responsiveRule;
 }
+
+export const useDynamicResponsiveValue = () => {
+  const order = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'];
+  const breakpoint = useResponsiveStyle({
+    xs: 'xs',
+    sm: 'sm',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+    xxl: 'xxl',
+  });
+  return (dynamicObj : IResponsiveRule<any>) => {
+    // if the breakpoint value is defined, use that value
+    // if not, use value of breakpoint right before
+    // example: breakpoint = 'md';  ({ xs: 100, lg: 200 }) =>  100 (use xs value)
+    const orderIndex = order.indexOf(breakpoint);
+    for (let i=orderIndex; i>=0; i--) {
+      if (dynamicObj[order[i]]) {
+        return dynamicObj[order[i]];
+      }
+    }
+    return undefined;
+  };
+};
 
 export const useResponsiveStyle = (onResponsiveStyle : TOneStyle | string) : TOneStyle | string => {
   if (!onResponsiveStyle) return undefined;
